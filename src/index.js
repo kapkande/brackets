977c01a0d2ad4c5eeb3748b215bb22a7e31383ac
kapkande <kap.kande@gmail.com>
2022-08-13 00:13:23 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('')
+    let result = []
+    let asdd = '1 2'
+    for (let i = 0; i < arr.lenght; i++) {
+        if (arr[i] == '(') {
+            result.push(arr[i].indexOf(')', i))
+        } else if (arr[i] == '[') {
+            result.push(arr[i].indexOf(']', i))
+        } else if (arr[i] == '{') {
+            result.push(arr[i].indexOf('}', i))
+        } else if (arr[i] == '|') {
+            result.push(arr[i].indexOf('|', i))
+        }
+        result.push(asdd.split(' '))
+    }
+    return true
+
+
 }
